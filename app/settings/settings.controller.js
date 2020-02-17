c0ab0f209f35397097f8201f7a0f7b88e1b08c17
Joao Oliveira <j.andre.oliveira@gmail.com>
2020-02-17 14:13:58 +0100

    added settings.

--- app/settings/settings.controller.js
+++ app/settings/settings.controller.js
@@ -1,13 +1,24 @@
 const router = require('express').Router();
-const { SettingsModel } = require( "../db/models");
+const {SettingsModel} = require('../db/models');
 
 router.get('/', async (req, res) => {
   try {
     const document = await SettingsModel.findOne();
-    if (!document) return res.status(404).send({ msg: 'record not found' });
+    if (!document) return res.status(404).send({msg: 'record not found'});
     return res.send(document.toJSON());
   } catch (err) {
-    return res.send({ error: err });
+    return res.send({error: err});
+  }
+});
+
+router.put('/', async (req, res) => {
+  try {
+
+    const document = await SettingsModel.updateOne({}, req.body);
+    if (!document) return res.status(404).send({msg: 'record not found'});
+    return res.send(document.toJSON());
+  } catch (err) {
+    return res.send({error: err});
   }
 });
 
---
+++ app/test/settings/settings.controller.test.js
@@ -0,0 +1,73 @@
+const {connect, closeDatabase, clearDatabase} = require('../test-db');
+const {SettingsModel} = require('../../db/models');
+const app = require('../../app');
+const request = require('supertest');
+
+const {expect} = require('chai');
+
+describe('GET /Settings', () => {
+  before(async () => await connect());
+
+  const settings = {
+    availableSizes: ['S', 'XL'],
+    productDefaultPrice: 5,
+    stampingExtraCost: 12
+  };
+
+  beforeEach(async () => {
+    await SettingsModel.create(settings);
+  });
+
+  afterEach(async () => {
+    await clearDatabase();
+  });
+
+  after(async () => await closeDatabase());
+
+  it('should retrieve settings', async () => {
+    const res = await request(app).get('/settings');
+    expect(res.status).eq(200);
+    expect(res.body).to.eql({
+      availableSizes: ['S', 'XL'],
+      productDefaultPrice: 5,
+      stampingExtraCost: 12
+    });
+  });
+
+  it('should update available sizes', async () => {
+    await request(app)
+      .put('/settings')
+      .send({availableSizes: ['XS']})
+      .expect(200);
+
+    const doc = await SettingsModel.findOne();
+    expect(doc.toJSON().availableSizes).eql(['XS']);
+  });
+
+  it('should update only default price', async () => {
+    await request(app)
+      .put('/settings')
+      .send({productDefaultPrice: 5})
+      .expect(200);
+
+    const doc = await SettingsModel.findOne();
+    const data = doc.toJSON();
+    expect(data.availableSizes).eql(settings.availableSizes);
+    expect(data.productDefaultPrice).eq(5);
+  });
+
+  it('should update stamping extra cost', async () => {
+    await request(app)
+      .put('/settings')
+      .send({stampingExtraCost: 5})
+      .expect(200);
+
+    const doc = await SettingsModel.findOne();
+    const data = doc.toJSON();
+    expect(data.availableSizes).eql(settings.availableSizes);
+    expect(data.productDefaultPrice).eq(settings.productDefaultPrice);
+    expect(data.stampingExtraCost).eq(5);
+  });
+
+
+});
